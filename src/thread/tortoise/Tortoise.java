46d77cd46e54cac31912937270b5bb9e231ae62f
kuldeep506 <67869264+kuldeep506@users.noreply.github.com>
2020-10-29 22:45:31 +0530

    creating constructor in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -5,4 +5,8 @@ public class Tortoise extends Thread{
     private static final int MILESTONES = 5;
 
     private final Thread tortoise;
+    private Tortoise(){
+        this.tortoise=new Thread(this,"tortoise");
+
+    }
 }
