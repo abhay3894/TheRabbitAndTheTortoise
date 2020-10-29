13875e997115232c1f50e3ccfef165ca0de10562
kuldeep506 <67869264+kuldeep506@users.noreply.github.com>
2020-10-29 22:53:57 +0530

    overriding run method for Tortoise

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,12 +1,21 @@
 package thread.tortoise;
 
-public class Tortoise extends Thread{
+public class Tortoise extends Thread {
 
     private static final int MILESTONES = 5;
 
     private final Thread tortoise;
-    private Tortoise(){
-        this.tortoise=new Thread(this,"tortoise");
 
-    }
+    private Tortoise() {this.tortoise = new Thread(this, "tortoise");}
+
+    public Thread getTortoise() { return tortoise;}
+
+    @Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Rabbit has started from the start line!");
+                    break;
 }
+
